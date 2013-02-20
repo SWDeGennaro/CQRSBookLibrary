d808f3885ad7ff7cac45af74d326f539a74a3cd2
Stephen De Gennaro <ste.degennaro@live.co.uk>
2013-02-20 19:40:23 +0000

    Deleting files that should only be local and not in the repository.

---
+++ BookLibrary.Tests.Unit/Events/All_domain_events_must_be_serializable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace BookLibrary.Tests.Unit.Events
+{
+    [TestFixture]
+    public class All_domain_events_must_be_serializable
+    {
+        [Test]
+        public void All_domain_events_will_have_the_Serializable_attribute_assigned()
+        {
+            var domainEventTypes = 
+                typeof(global::BookLibrary.Events.DomainEvent)
+                .Assembly
+                .GetExportedTypes()
+                .Where(x => x.BaseType == typeof(global::BookLibrary.Events.DomainEvent))
+                .ToList();
+
+            foreach (var domainEventType in domainEventTypes)
+            {
+                if (domainEventType.IsSerializable)
+                    continue;
+
+                throw new Exception(string.Format("Domain event '{0}' is not Serializable", domainEventType.FullName));
+            }
+        }
+    }
+}
